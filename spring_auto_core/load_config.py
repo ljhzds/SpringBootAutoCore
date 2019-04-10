e007787fda29220c7217dbfeb319101b96193da0
ljhzds <136766249@qq.com>
2019-04-10 15:25:39 +0800

    v0.1

--- spring_auto_core/load_config.py
+++ spring_auto_core/load_config.py
@@ -1,26 +1,22 @@
 # coding: utf-8
 from read_config_excel import read_app_config
+from secret_remote import JDBC_URL, DB_DRIVER, DB_PASSWD, DB_USER
 
 # 工程名
 APP_NAME = 'pySpringAutoDemo'
 API_VERSION = 'v1'
 # 工程包名
-APP_GROUP_ID = 'com.guangfa'
+APP_GROUP_ID = 'com.zhangdesheng'
 APP_ART_ID = 'autodemo'
-APP_PACKAGE_NAME = 'com.guangfa.autodemo'
+APP_PACKAGE_NAME = 'com.zhangdesheng.autodemo'
 APP_DESC = 'python generator demo'
 # 工程保存目录
 CODE_SAVE_ROOT = 'C:\\spring_learning\\'
-# CODE_SAVE_ROOT_TMP = '/tmp/springauto/'
-CODE_SAVE_ROOT_TMP = CODE_SAVE_ROOT
+CODE_SAVE_ROOT_TMP = '/tmp/springauto/'
+# CODE_SAVE_ROOT_TMP = CODE_SAVE_ROOT
 
 SPRINGBOOT_VERSION = '1.5.19.RELEASE'
 JAVA_VERSION = '1.8'
-
-JDBC_URL = 'jdbc:mysql://119.29.103.193:3306/easyweb?useUnicode=true&characterEncoding=utf8'
-DB_USER = 'zdstest'
-DB_PASSWD = 'SFD_SF3437lk'
-DB_DRIVER = 'com.mysql.jdbc.Driver'
 
 
 def get_all_config(desc_excel_file_path=None, is_web_app=False):
---
+++ spring_auto_core/secret_remote.py
@@ -0,0 +1,4 @@
+JDBC_URL = 'jdbc:mysql://xx.xx.xxx.xxx:3306/easyweb?useUnicode=true&characterEncoding=utf8'
+DB_USER = 'xxx'
+DB_PASSWD = 'xxx'
+DB_DRIVER = 'com.mysql.jdbc.Driver'
